c83335525b61ade0478ddae2af39a4dc083b0293
iam-dha <iamdha0706@gmail.com>
2025-05-21 00:12:42 +0700

    complete post, role, and user admin api

--- BE/controllers/admin/role.controller.js
+++ BE/controllers/admin/role.controller.js
@@ -15,6 +15,62 @@ module.exports.getAllRoles = async (req, res) => {
     }
 }
 
+// [GET] /api/v1/admin/roles/permissions
+module.exports.getAllPermissions = async (req, res) => {
+    try {
+        const permissions = await Permission.find({ deleted: false }).select("title description -_id");
+        if (!permissions || permissions.length === 0) {
+            return res.status(404).json({ message: "No permissions found" });
+        }
+        return res.status(200).json({
+            result: permissions.length,
+            data: permissions,
+        });
+    } catch (error) {
+        console.error('[GET /api/v1/admin/roles/permissions] Error:', error);
+        return res.status(500).json({ message: "Internal server error" });
+    }
+}
+
+// [PATCH] /api/v1/admin/roles/permissions
+module.exports.updateRolePermissions = async (req, res) => {
+    const {rolePermissions} = req.body;
+    try {
+        for (const role of rolePermissions) {
+            const roleDoc = await Role.findOne({ _id: role._id, title: role.title, deleted: false });
+            if (!roleDoc) {
+                return res.status(404).json({ message: `Role with ID ${role.roleId} not found` });
+            }
+            roleDoc.permissions = role.permissions;
+            await roleDoc.save();
+        }
+        return res.status(200).json({
+            message: "Update role permissions successfully",
+        });
+    } catch (error) {
+        console.error(`[PATCH /api/v1/admin/roles/permissions] Error:`, error);
+        return res.status(500).json({ message: "Internal server error" });
+    }
+}
+
+// [GET] /api/v1/admin/roles/:roleId
+module.exports.getRole = async (req, res) => {
+    const { roleId } = req.params;
+    try {
+        const role = await Role.findOne({ _id: roleId, deleted: false });
+        if (!role) {
+            return res.status(404).json({ message: "Role not found" });
+        }
+        return res.status(200).json({
+            message: "Get role successfully",
+            data: role,
+        });
+    } catch (error) {
+        console.error(`[GET /api/v1/admin/roles/${roleId}] Error:`, error);
+        return res.status(500).json({ message: "Internal server error" });
+    }
+}
+
 // [POST] /api/v1/admin/roles
 module.exports.createRole = async (req, res) => {
     const { title, description = ""} = req.body;
@@ -61,62 +117,6 @@ module.exports.deleteRole = async (req, res) => {
         });
     } catch (error) {
         console.error(`[DELETE /api/v1/admin/roles/${roleId}] Error:`, error);
-        return res.status(500).json({ message: "Internal server error" });
-    }
-}
-
-// [GET] /api/v1/admin/roles/permissions
-module.exports.getAllPermissions = async (req, res) => {
-    try {
-        const permissions = await Permission.find({ deleted: false }).select("title description -_id");
-        if (!permissions || permissions.length === 0) {
-            return res.status(404).json({ message: "No permissions found" });
-        }
-        return res.status(200).json({
-            result: permissions.length,
-            data: permissions,
-        });
-    } catch (error) {
-        console.error('[GET /api/v1/admin/roles/permissions] Error:', error);
-        return res.status(500).json({ message: "Internal server error" });
-    }
-}
-
-// [GET] /api/v1/admin/roles/:roleId
-module.exports.getRole = async (req, res) => {
-    const { roleId } = req.params;
-    try {
-        const role = await Role.findOne({ _id: roleId, deleted: false });
-        if (!role) {
-            return res.status(404).json({ message: "Role not found" });
-        }
-        return res.status(200).json({
-            message: "Get role successfully",
-            data: role,
-        });
-    } catch (error) {
-        console.error(`[GET /api/v1/admin/roles/${roleId}] Error:`, error);
-        return res.status(500).json({ message: "Internal server error" });
-    }
-}
-
-// [PATCH] /api/v1/admin/roles/permissions
-module.exports.updateRolePermissions = async (req, res) => {
-    const {rolePermissions} = req.body;
-    try {
-        for (const role of rolePermissions) {
-            const roleDoc = await Role.findOne({ _id: role._id, title: role.title, deleted: false });
-            if (!roleDoc) {
-                return res.status(404).json({ message: `Role with ID ${role.roleId} not found` });
-            }
-            roleDoc.permissions = role.permissions;
-            await roleDoc.save();
-        }
-        return res.status(200).json({
-            message: "Update role permissions successfully",
-        });
-    } catch (error) {
-        console.error(`[PATCH /api/v1/admin/roles/permissions] Error:`, error);
         return res.status(500).json({ message: "Internal server error" });
     }
 }
