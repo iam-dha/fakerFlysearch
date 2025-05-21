d49371614129035a1bc62b2d5936691a59f8ef8f
iam-dha <iamdha0706@gmail.com>
2025-05-21 14:01:57 +0700

    add promotion model

---
+++ BE/models/promotion.model.js
@@ -0,0 +1,66 @@
+const mongoose = require("mongoose");
+const slug = require("mongoose-slug-updater");
+mongoose.plugin(slug);
+const promotion_schema = new mongoose.Schema({
+    label: {
+        type: String,
+        required: true,
+    },
+    description: {
+        type: String,
+        default: "",
+    },
+    code: {
+        type: String,
+        required: true,
+    },
+    thumbnail: {
+        type: String,
+        default: "",
+    },
+    start_date: {
+        type: Date,
+        default: Date.now,
+    },
+    end_date: {
+        type: Date,
+        default: Date.now + 7 * 24 * 60 * 60 * 1000,
+    },
+    totalSlot: {
+        type: Number,
+        default: 0,
+    },
+    isIncluded: {
+        type: Boolean,
+        default: false,
+    },
+    isActive: {
+        type: Boolean,
+        enum: ["active", "inactive"],
+        default: true,
+    },
+    deleted: {
+        type: Boolean,
+        default: false,
+    },
+    deletedAt: {
+        type: Date,
+        default: null,
+    },
+    discount_value: {
+        type: Number,
+        default: 0,
+    },
+    slug: {
+        type: String,
+        slug: "label",
+        unique: true
+    }
+},
+    {
+        timestamps: true,
+    }
+);
+
+const Promotion = mongoose.model("Promotion", promotion_schema, "promotions");
+module.exports = Promotion;
