0c2308bb9baa810a189f5fbf21d0c554970c40cf
iam-dha <iamdha0706@gmail.com>
2025-05-26 12:36:16 +0700

    add promotion

--- BE/routes/client/index.routes.js
+++ BE/routes/client/index.routes.js
@@ -5,6 +5,7 @@ const flightRoute = require("./flight.routes");
 const hotelRoute = require("./hotel.routes");
 const carRoute = require("./car.routes");
 const bookingRoute = require("./booking.routes");
+const promotionRoute = require("./promotion.routes");
 module.exports = (app) => {
   app.use(`${systemConfig.apiPath}/v1/client/auth`, authRoute);
   app.use(`${systemConfig.apiPath}/v1/client/user`, userRoute);
@@ -12,5 +13,6 @@ module.exports = (app) => {
   app.use(`${systemConfig.apiPath}/v1/client/hotels`, hotelRoute);
   app.use(`${systemConfig.apiPath}/v1/client/cars`, carRoute);
   app.use(`${systemConfig.apiPath}/v1/client/bookings`, bookingRoute);
+  app.use(`${systemConfig.apiPath}/v1/client/promotions`, promotionRoute);
 };
 
